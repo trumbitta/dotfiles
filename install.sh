ab4af858046be8f0acf412dfd9a7d11e1a1bebf1
William Ghelfi <trumbitta@gmail.com>
2022-01-17 15:34:58 +0000

    feat: configure zsh

--- install.sh
+++ install.sh
@@ -6,5 +6,11 @@ if test ! -e /ide/bin/gp-code || test ! -v GITPOD_REPO_ROOT; then {
     printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
 } fi
 
+# git 
+
 SOURCE_DIR="$(readlink -f "$0")" && SOURCE_DIR="${SOURCE_DIR%/*}";
 cat $SOURCE_DIR/.gitconfig >> $HOME/.gitconfig
+
+# zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
