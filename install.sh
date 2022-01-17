c9d6cb8b542228bcb529fd2271aee62a0f372f54
William Ghelfi <trumbitta@gmail.com>
2022-01-17 16:08:07 +0100

    build: add install script

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ if test ! -e /ide/bin/gp-code || test ! -v GITPOD_REPO_ROOT; then {
     printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
 } fi
 
-SOURCE_DIR="$(readlink -f "$0")" && _source_dir="${_source_dir%/*}";
+SOURCE_DIR="$(readlink -f "$0")" && _source_dir="${SOURCE_DIR%/*}";
 cat $SOURCE_DIR/.dotfiles/.gitconfig >> $HOME/.gitconfig
