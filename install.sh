32dd8bf2bdf68117b91d14122a52bb06dd4cbeb1
William Ghelfi <trumbitta@gmail.com>
2022-02-04 10:04:21 +0100

    build(gitpod): update guard for Gitpod environment

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -eu;
 
-if test ! -e /ide/bin/gp-code || test ! -v GITPOD_REPO_ROOT; then {
+if test ! -e /ide/bin/remote-cli/gp-code || test ! -v GITPOD_REPO_ROOT; then {
     printf 'error: This script is meant to be run on Gitpod, quitting...\n' && exit 1;
 } fi
 
